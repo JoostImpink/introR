293a548e1f9d73a091292076800f59b896907a65
Joost <joostimpink@gmail.com>
2015-11-04 14:20:46 -0500

    bug fix myHighLow.r

--- chapter15/myHighLow.r
+++ chapter15/myHighLow.r
@@ -3,7 +3,9 @@
 # activityVar: column name of activity variable (e.g. "orders")
 # costVar: column name of cost variable (e.g. "costs")
 myHighLow <- function (matrix, activityVar, costVar){ 	
-
+	# get orders/activity
+	orders <- myMatrix[ , activityVar]
+	# compute difference in costs, and difference in activity
 	diffCosts <- matrix[which.max(orders), costVar] - matrix[which.min(orders), costVar] 
 	diffActivity <- matrix[which.max(orders), activityVar] - matrix[which.min(orders), activityVar]
 	# slope 
